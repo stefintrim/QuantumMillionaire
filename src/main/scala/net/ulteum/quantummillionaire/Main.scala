7fc96b7ef938f89a120e05095b855f0fa952b3e9
stefintrim <stefancompton23@gmail.com>
2022-04-27 15:46:39 +0100

    Rename classes

--- src/main/scala/net/ulteum/quantummillionaire/Main.scala
+++ src/main/scala/net/ulteum/quantummillionaire/Main.scala
@@ -3,5 +3,5 @@ package net.ulteum.quantummillionaire
 import cats.effect.{IO, IOApp}
 
 object Main extends IOApp.Simple {
-  def run:IO[Unit] = QuantummillionaireServer.run
+  def run:IO[Unit] = QuantumMillionaireServer.run
 }
