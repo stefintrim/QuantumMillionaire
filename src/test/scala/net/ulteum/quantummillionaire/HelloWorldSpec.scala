22bdb44ab763efbf7b257701786cdc77ac7c4eb9
stefintrim <stefancompton23@gmail.com>
2022-04-27 15:47:28 +0100

    Rename classes

--- src/test/scala/net/ulteum/quantummillionaire/HelloWorldSpec.scala
+++ src/test/scala/net/ulteum/quantummillionaire/HelloWorldSpec.scala
@@ -7,17 +7,17 @@ import munit.CatsEffectSuite
 
 class HelloWorldSpec extends CatsEffectSuite {
 
+  private[this] val retHelloWorld: IO[Response[IO]] = {
+    val getHW = Request[IO](Method.GET, uri"/hello/world")
+    val helloWorld = Lotto.impl
+    QuantumMillionaireRoutes.helloWorldRoutes(helloWorld).orNotFound(getHW)
+  }
+
   test("HelloWorld returns status code 200") {
     assertIO(retHelloWorld.map(_.status) ,Status.Ok)
   }
 
   test("HelloWorld returns hello world message") {
     assertIO(retHelloWorld.flatMap(_.as[String]), "{\"message\":\"Hello, world\"}")
-  }
-
-  private[this] val retHelloWorld: IO[Response[IO]] = {
-    val getHW = Request[IO](Method.GET, uri"/hello/world")
-    val helloWorld = Lotto.impl
-    QuantummillionaireRoutes.helloWorldRoutes(helloWorld).orNotFound(getHW)
   }
 }
